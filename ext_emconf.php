e3146dd27bb05e141f188745cd6e6c0daa656f4f
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2018-11-23 09:11:17 +0100

    [BREAKING] Add 8LTS compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,39 @@
 <?php
 
-/**
- * *************************************************************
- * Update of "kb_tv_cont_slide" to work with templavoilaplus
- * Original author:
+/***************************************************************
+ *  Copyright notice
  *
- * 'author' => 'Bernhard Kraft',
- * 'author_email' => 'kraftb@think-open.at',
- * *************************************************************
- */
-$EM_CONF[$_EXTKEY] = array(
+ *  Update of "kb_tv_cont_slide" to work with templavoilaplus
+ *  Original author:
+ *  (c) 2004-2014 Bernhard Kraft (kraftb@think-open.at)
+ *
+ *  Current maintainer:
+ *  (c) 2016-2018 J. Peter M. Schuler (j.peter.m.schuler@uni-due.de)
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+$EM_CONF['tvplus_contentslide'] = [
     'title' => 'TemplaVoila Plus: Content Slide',
     'description' => 'This extension allows you to inherit the content of a TemplaVoilaPlus content element column to its child pages - Adaption of EXT:kb_tv_cont_slide to work with templavoilaplus',
     'category' => 'plugin',
-    'version' => '1.1.4-alpha',
+    'version' => '2.0.0',
     'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
@@ -21,13 +41,12 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Schuler, J. Peter M.',
     'author_email' => 'j.peter.m.schuler@uni-due.de',
     'author_company' => '',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'php' => '5.2.0-0.0.0',
-            'typo3' => '6.2.0-8.7.99'
-        ),
-        'conflicts' => array(),
-        'suggests' => array()
-    )
-);
-
+            'typo3' => '8.7.0-8.7.99'
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ]
+];
