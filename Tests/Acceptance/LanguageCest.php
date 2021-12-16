490d6b93f0e840e6dbff25f9239b34ecc403acef
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2021-12-16 14:13:26 +0100

    [FEATURE] add acceptance tests

---
+++ Tests/Acceptance/LanguageCest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jpmschuler\TvplusContentslide\Tests\Acceptance;
+
+/**
+ * Test case.
+ */
+class LanguageCest
+{
+    public function _before(\AcceptanceTester $I): void
+    {
+        $I->amOnPage('/');
+    }
+
+    public function seeSlideContent(\AcceptanceTester $I): void
+    {
+        $I->see('SlideContent');
+    }
+}
